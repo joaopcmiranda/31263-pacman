44719c230bf93120cc34bebff3388798162f790a
Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2023-10-06 13:09:43 +1100

    Added standing demo

--- Assets/Scripts/PacStudent/StandingDemo.cs
+++ Assets/Scripts/PacStudent/StandingDemo.cs
@@ -1,18 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StandingDemo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly int WalkingParam = Animator.StringToHash("walking");
+    private Animator m_Animator;
+    private static readonly int DirectionParam = Animator.StringToHash("direction");
+
+    private void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Animator.SetBool(WalkingParam, false);
+        StartCoroutine(CycleDirections());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CycleDirections()
     {
-        
+        while (true)
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                m_Animator.SetInteger(DirectionParam, i);
+                yield return new WaitForSeconds(3);
+            }
+        }
     }
 }
